6e4b00f72bc1a955ceba174e1d9fd36276a0c68e
klchen0112 <klchen0112@gmail.com>
2023-11-18 23:08:09 +0800

    remote outbound any

--- clash_to_sing_box/run.py
+++ clash_to_sing_box/run.py
@@ -172,353 +172,347 @@ zju_domains = [
 ]
 
 log_settings = {
-
     "disabled": False,
     "level": "warn",
     # "output": "box.log",
     "timestamp": True,
 }
 dns_settings = {
-        "servers": [
-            {"tag": "proxyDns", "address": "tls://8.8.8.8", "detour": "proxy"},
+    "servers": [
+        {"tag": "proxyDns", "address": "tls://8.8.8.8", "detour": "proxy"},
+        {
+            "tag": "localDns",
+            "address": "https://223.5.5.5/dns-query",
+            "detour": "direct",
+        },
+    ]
+    + (
+        []
+        if not use_zju
+        else [
             {
-                "tag": "localDns",
-                "address": "https://223.5.5.5/dns-query",
+                "tag": "zjuDns",
+                "address": zju_dns,
                 "detour": "direct",
             },
         ]
-        + (
-            []
-            if not use_zju
-            else [
-                {
-                    "tag": "zjuDns",
-                    "address": "10.10.0.21",
-                    "detour": "direct",
-                },
-            ]
-        )
-        + [
-            {"tag": "block", "address": "rcode://success"},
-            {"tag": "remote", "address": "fakeip", "detour": "proxy"},
-        ],
-        "rules": [
-            {"domain": ["ghproxy.com", "cdn.jsdelivr.net"], "server": "localDns"},
-            {
-                "geosite": "category-ads-all",
-                "server": "block",
-                "disable_cache": True,
-            },
-            {"outbound": "any", "server": "localDns", "disable_cache": True},
-        ]
-        + (
-            []
-            if not use_zju
-            else [
-                {
-                    "domain_suffix": zju_domains,
-                    "server": "zjuDns",
-                },
-            ]
-        )
-        + [
-            {"geosite": ["cn", "private", "category-games@cn"], "server": "localDns"},
-            {"clash_mode": "direct", "server": "localDns"},
-            {"clash_mode": "global", "server": "proxyDns"},
-            {"geosite": "geolocation-!cn", "server": "proxyDns"},
-            {"query_type": ["A", "AAAA"], "server": "remote"},
-            {"outbound": ["any"], "server": "remote"},
-        ],
-        "fakeip": {
-            "enabled": True,
-            "inet4_range": "198.18.0.0/15",
-            **({"inet6_range": "fc00::/18"} if args.six else {}),
-        },
-        "independent_cache": True,
-        "strategy": "ipv4_only",
-}
-inbounds_settings = [{
-            "type": "tun",
-            "inet4_address": "172.19.0.1/30",
-            **({"inet6_range": "fdfd:9527::1/32"} if args.six else {}),
-            "mtu": 9000,
-            "auto_route": True,
-            "strict_route": True,
-            "sniff": True,
-            "endpoint_independent_nat": False,
-            "stack": "mixed",
-            "platform": {
-                "http_proxy": {
-                    "enabled": True,
-                    "server": "127.0.0.1",
-                    "server_port": 7890,
-                }
-            },
-        },
+    )
+    + [
+        {"tag": "block", "address": "rcode://success"},
+        {"tag": "remote", "address": "fakeip", "detour": "proxy"},
+    ],
+    "rules": [
+        {"domain": ["ghproxy.com", "cdn.jsdelivr.net"], "server": "localDns"},
         {
-            "type": "mixed",
-            "listen": "127.0.0.1",
-            "listen_port": 7890,
-            "sniff": True,
+            "geosite": "category-ads-all",
+            "server": "block",
+            "disable_cache": True,
         },
+        {"outbound": "any", "server": "localDns", "disable_cache": True},
     ]
+    + (
+        []
+        if not use_zju
+        else [
+            {
+                "domain_suffix": zju_domains,
+                "server": "zjuDns",
+            },
+        ]
+    )
+    + [
+        {"geosite": ["cn", "private", "category-games@cn"], "server": "localDns"},
+        {"clash_mode": "direct", "server": "localDns"},
+        {"clash_mode": "global", "server": "proxyDns"},
+        {"geosite": "geolocation-!cn", "server": "proxyDns"},
+        {"query_type": ["A", "AAAA"], "server": "remote"},
+        # {"outbound": ["any"], "server": "remote"},
+    ],
+    "fakeip": {
+        "enabled": True,
+        "inet4_range": "198.18.0.0/15",
+        **({"inet6_range": "fc00::/18"} if args.six else {}),
+    },
+    "independent_cache": True,
+    "strategy": "ipv4_only",
+}
+inbounds_settings = [
+    {
+        "type": "tun",
+        "inet4_address": "172.19.0.1/30",
+        **({"inet6_range": "fdfd:9527::1/32"} if args.six else {}),
+        "mtu": 9000,
+        "auto_route": True,
+        "strict_route": True,
+        "sniff": True,
+        "endpoint_independent_nat": False,
+        "stack": "system",
+        "platform": {
+            "http_proxy": {
+                "enabled": True,
+                "server": "127.0.0.1",
+                "server_port": 7890,
+            }
+        },
+    },
+]
 
 outbounds_settings = [
-        {
-            "tag": "proxy",
-            "type": "selector",
-            "outbounds": (["高速节点"] if high_speed is not None else [])
-            + ["auto", "地区选择", "节点选择", "direct"],
-        },
-        {
-            "tag": "广告过滤",
-            "type": "selector",
-            "outbounds": ["block", "direct"],
-        },
-        {
-            "tag": "学术",
-            "type": "selector",
-            "outbounds": ["proxy", "节点选择", "direct"],
-        },
-        {
-            "tag": "OpenAI",
-            "type": "selector",
-            "outbounds": [
-                "美国",
-                "proxy",
-            ],
-        },
-        {
-            "tag": "Developer",
-            "type": "selector",
-            "outbounds": [
-                "proxy",
-                "direct",
-            ],
-        },
-        {
-            "tag": "OneDrive",
-            "type": "selector",
-            "outbounds": ["auto", "proxy", "direct"],
-        },
-        {
-            "tag": "Microsoft",
-            "type": "selector",
-            "outbounds": [
-                "direct",
-                "proxy",
-            ],
-        },
-        {
-            "tag": "Social",
-            "type": "selector",
-            "outbounds": ["auto", "proxy", "direct"],
-        },
-        {
-            "tag": "Shopping",
-            "type": "selector",
-            "outbounds": ["proxy", "direct"],
-        },
-        {
-            "tag": "哔哩哔哩",
-            "type": "selector",
-            "outbounds": [
-                "direct",
-                "台湾",
-                "香港",
-                "proxy",
-            ],
-        },
-        {
-            "tag": "巴哈姆特",
-            "type": "selector",
-            "outbounds": [
-                "台湾",
-                "香港",
-                "proxy",
-            ],
-        },
-        {
-            "tag": "Apple",
-            "type": "selector",
-            "outbounds": [
-                "direct",
-                "proxy",
-            ],
-        },
-        {
-            "tag": "Game",
-            "type": "selector",
-            "outbounds": [
-                "日本",
-                "香港",
-                "台湾",
-                "proxy",
-                "direct",
-            ],
-        },
-        {
-            "tag": "Streaming",
-            "type": "selector",
-            "outbounds": ["auto", "proxy", "direct"],
-        },
-        {
-            "tag": "Google",
-            "type": "selector",
-            "outbounds": ["proxy", "direct"],
-        },
-        {"tag": "Speedtest", "type": "selector", "outbounds": ["direct", "proxy"]},
-        {"tag": "not cn", "type": "selector", "outbounds": ["proxy", "direct"]},
-        {"type": "direct", "tag": "direct"},
-        {"type": "dns", "tag": "dns-out"},
-        {"type": "block", "tag": "block"},
-    ]
+    {
+        "tag": "proxy",
+        "type": "selector",
+        "outbounds": (["高速节点"] if high_speed is not None else [])
+        + ["auto", "地区选择", "节点选择", "direct"],
+    },
+    {
+        "tag": "广告过滤",
+        "type": "selector",
+        "outbounds": ["block", "direct"],
+    },
+    {
+        "tag": "学术",
+        "type": "selector",
+        "outbounds": ["proxy", "节点选择", "direct"],
+    },
+    {
+        "tag": "OpenAI",
+        "type": "selector",
+        "outbounds": [
+            "美国",
+            "proxy",
+        ],
+    },
+    {
+        "tag": "Developer",
+        "type": "selector",
+        "outbounds": [
+            "proxy",
+            "direct",
+        ],
+    },
+    {
+        "tag": "OneDrive",
+        "type": "selector",
+        "outbounds": ["auto", "proxy", "direct"],
+    },
+    {
+        "tag": "Microsoft",
+        "type": "selector",
+        "outbounds": [
+            "direct",
+            "proxy",
+        ],
+    },
+    {
+        "tag": "Social",
+        "type": "selector",
+        "outbounds": ["auto", "proxy", "direct"],
+    },
+    {
+        "tag": "Shopping",
+        "type": "selector",
+        "outbounds": ["proxy", "direct"],
+    },
+    {
+        "tag": "哔哩哔哩",
+        "type": "selector",
+        "outbounds": [
+            "direct",
+            "台湾",
+            "香港",
+            "proxy",
+        ],
+    },
+    {
+        "tag": "巴哈姆特",
+        "type": "selector",
+        "outbounds": [
+            "台湾",
+            "香港",
+            "proxy",
+        ],
+    },
+    {
+        "tag": "Apple",
+        "type": "selector",
+        "outbounds": [
+            "direct",
+            "proxy",
+        ],
+    },
+    {
+        "tag": "Game",
+        "type": "selector",
+        "outbounds": [
+            "日本",
+            "香港",
+            "台湾",
+            "proxy",
+            "direct",
+        ],
+    },
+    {
+        "tag": "Streaming",
+        "type": "selector",
+        "outbounds": ["auto", "proxy", "direct"],
+    },
+    {
+        "tag": "Google",
+        "type": "selector",
+        "outbounds": ["proxy", "direct"],
+    },
+    {"tag": "Speedtest", "type": "selector", "outbounds": ["direct", "proxy"]},
+    {"tag": "not cn", "type": "selector", "outbounds": ["proxy", "direct"]},
+    {"type": "direct", "tag": "direct"},
+    {"type": "dns", "tag": "dns-out"},
+    {"type": "block", "tag": "block"},
+]
 
 route_settings = {
-        "auto_detect_interface": True,
-        "final": "proxy",
-        "geoip": {
-            "download_url": "https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geoip.db",
-            "download_detour": "direct",
+    "auto_detect_interface": True,
+    "final": "proxy",
+    "geoip": {
+        "download_url": "https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geoip.db",
+        "download_detour": "direct",
+    },
+    "geosite": {
+        "download_url": "https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geosite.db",
+        "download_detour": "direct",
+    },
+    "rules": [
+        {
+            "type": "logical",
+            "mode": "or",
+            "rules": [
+                {
+                    "protocol": "dns",
+                    "port": [53],
+                }
+            ],
+            "outbound": "dns-out",
         },
-        "geosite": {
-            "download_url": "https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geosite.db",
-            "download_detour": "direct",
+        {"network": "udp", "port": 443, "outbound": "block"},
+        {"geosite": "category-ads-all", "outbound": "广告过滤"},
+        {"clash_mode": "direct", "outbound": "direct"},
+        {"clash_mode": "global", "outbound": "proxy"},
+        {
+            "domain": [
+                "clash.razord.top",
+                "yacd.metacubex.one",
+                "yacd.haishan.me",
+                "d.metacubex.one",
+            ],
+            "outbound": "direct",
         },
-        "rules": [
+    ]
+    + (
+        []
+        if not use_zju
+        else [
             {
-                "type": "logical",
-                "mode": "or",
-                "rules": [
-                    {
-                        "protocol": "dns",
-                        "port": [53],
-                    }
-                ],
-                "outbound": "dns-out",
-            },
-            {"network": "udp", "port": 443, "outbound": "block"},
-            {"geosite": "category-ads-all", "outbound": "广告过滤"},
-            {"clash_mode": "direct", "outbound": "direct"},
-            {"clash_mode": "global", "outbound": "proxy"},
-            {
-                "domain": [
-                    "clash.razord.top",
-                    "yacd.metacubex.one",
-                    "yacd.haishan.me",
-                    "d.metacubex.one",
-                ],
+                "domain_suffix": zju_domains,
                 "outbound": "direct",
             },
         ]
-        + (
-            []
-            if not use_zju
-            else [
-                {
-                    "domain_suffix": zju_domains,
-                    "outbound": "direct",
-                },
-            ]
-        )
-        + [
+    )
+    + [
+        {
+            "domain_suffix": [
+                "canvas-user-content.com",
+                "iclicker.com",
+                "emerald.com",
+                "ieee.org",
+                "proquest.com",
+                "sciencedirect.com",
+                "nature.com",
+                "acs.org",
+                "taylorfrancis.com",
+                "osapublishing.org",
+                "clarivate.com",
+                "gale.com",
+                "worldscientific.com",
+                "siam.org",
+                "ascelibrary.org",
+                "scitation.org",
+                "wiley.com",
+            ],
+            "geosite": "category-scholar-!cn",
+            "outbound": "学术",
+        },
+        {
+            "domain_keyword": ["speedtest"],
+            "domain_suffix": ["cdnst.net", "ziffstatic.com"],
+            "outbound": "Speedtest",
+        },
+        {"geosite": "openai", "outbound": "OpenAI"},
+        {"geosite": "category-dev-cn", "outbound": "direct"},
+        {
+            "geosite": ["category-dev", "category-container"],
+            "outbound": "Developer",
+        },
+        {"geosite": ["google"], "outbound": "Google"},
+        {
+            "geosite": ["category-social-media-cn"],
+            "outbound": "direct",
+        },
+        {
+            "geosite": ["category-social-media-!cn", "category-communication"],
+            "outbound": "Social",
+        },
+        {"geosite": "amazon", "outbound": "Shopping"},
+        {"geosite": "apple", "outbound": "Apple"},
+        {"geosite": "microsoft", "outbound": "Microsoft"},
+        {"geosite": "category-games@cn", "outbound": "direct"},
+        {"geosite": "category-games", "outbound": "Game"},
+        {
+            "geosite": "bilibili",
+            "outbound": "哔哩哔哩",
+        },
+        {
+            "geosite": "bahamut",
+            "outbound": "巴哈姆特",
+        },
+        {
+            "geosite": [
+                "tiktok",
+                "youtube",
+                "netflix",
+                "hbo",
+                "disney",
+                "primevideo",
+                "category-media",
+                "category-entertainment",
+            ],
+            "outbound": "Streaming",
+        },
+        {"geosite": "geolocation-!cn", "outbound": "not cn"},
+    ]
+    + (
+        []
+        if not use_zju
+        else [
             {
-                "domain_suffix": [
-                    "canvas-user-content.com",
-                    "iclicker.com",
-                    "emerald.com",
-                    "ieee.org",
-                    "proquest.com",
-                    "sciencedirect.com",
-                    "nature.com",
-                    "acs.org",
-                    "taylorfrancis.com",
-                    "osapublishing.org",
-                    "clarivate.com",
-                    "gale.com",
-                    "worldscientific.com",
-                    "siam.org",
-                    "ascelibrary.org",
-                    "scitation.org",
-                    "wiley.com",
-                ],
-                "geosite": "category-scholar-!cn",
-                "outbound": "学术",
-            },
-            {
-                "domain_keyword": ["speedtest"],
-                "domain_suffix": ["cdnst.net", "ziffstatic.com"],
-                "outbound": "Speedtest",
-            },
-            {"geosite": "openai", "outbound": "OpenAI"},
-            {"geosite": "category-dev-cn", "outbound": "direct"},
-            {
-                "geosite": ["category-dev", "category-container"],
-                "outbound": "Developer",
-            },
-            {"geosite": ["google"], "outbound": "Google"},
-            {
-                "geosite": ["category-social-media-cn"],
+                "ip_cidr": ["10.0.0.0/8"],
                 "outbound": "direct",
             },
-            {
-                "geosite": ["category-social-media-!cn", "category-communication"],
-                "outbound": "Social",
-            },
-            {"geosite": "amazon", "outbound": "Shopping"},
-            {"geosite": "apple", "outbound": "Apple"},
-            {"geosite": "microsoft", "outbound": "Microsoft"},
-            {"geosite": "category-games@cn", "outbound": "direct"},
-            {"geosite": "category-games", "outbound": "Game"},
-            {
-                "geosite": "bilibili",
-                "outbound": "哔哩哔哩",
-            },
-            {
-                "geosite": "bahamut",
-                "outbound": "巴哈姆特",
-            },
-            {
-                "geosite": [
-                    "tiktok",
-                    "youtube",
-                    "netflix",
-                    "hbo",
-                    "disney",
-                    "primevideo",
-                    "category-media",
-                    "category-entertainment",
-                ],
-                "outbound": "Streaming",
-            },
-            {"geosite": "geolocation-!cn", "outbound": "not cn"},
         ]
-        + (
-            []
-            if not use_zju
-            else [
-                {
-                    "ip_cidr": ["10.0.0.0/8"],
-                    "outbound": "direct",
-                },
-            ]
-        )
-        + [
-            {"geoip": "google", "outbound": "Google"},
-            {
-                "geoip": ["telegram", "twitter", "facebook"],
-                "outbound": "Social",
-            },
-            {
-                "geoip": "netflix",
-                "outbound": "Streaming",
-            },
-            {
-                "geoip": ["private", "cn"],
-                "outbound": "direct",
-            },
-        ],
-    }
+    )
+    + [
+        {"geoip": "google", "outbound": "Google"},
+        {
+            "geoip": ["telegram", "twitter", "facebook"],
+            "outbound": "Social",
+        },
+        {
+            "geoip": "netflix",
+            "outbound": "Streaming",
+        },
+        {
+            "geoip": ["private", "cn"],
+            "outbound": "direct",
+        },
+    ],
+}
 
 result_json = {
     "log": log_settings,
