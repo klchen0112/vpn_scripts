cee88ff0e4c8c266bff09ce0ab3745b9feb429a9
klchen0112 <klchen0112@gmail.com>
2024-01-12 20:08:17 +0800

    加入devenv

---
+++ devenv.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    # version = "3.11.3";
+
+    venv.enable = true;
+    venv.requirements = ./requirements.txt;
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
