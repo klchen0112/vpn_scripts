f02a425f8a6fa050d769b3aae3c2ccd5c2fd6bbb
klchen0112 <klchen0112@gmail.com>
2025-05-03 21:05:42 +0800

    update mosdns

--- mosdns/getrule.sh
+++ mosdns/getrule.sh
@@ -32,6 +32,6 @@ geodat_update
 v2dat_dump
 # update_local_ptr
 
-# touch $v2dat_dir/rules/force-nocn.txt
-# touch $v2dat_dir/rules/force-cn.txt
+touch $v2dat_dir/rules/force-nocn.txt
+touch $v2dat_dir/rules/force-cn.txt
 # force-cn 是强制本地解析域名，force-nocn 是强制非本地解析域名
